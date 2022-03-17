170a9ce0a1fd1c51b0d139bf735481afc4e87522
pedro imbrech <elprogramadorpedro@gmail.com>
2022-03-17 17:37:54 -0300

    rev

--- src/Componentes/Imagem.js
+++
@@ -1,6 +0,0 @@
-export default function Imagem(props){
-    const src = props.src;
-    return (
-        <img src={src} />
-    )
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDom from "react-dom"
-import App from "./Componentes/App"
-
-const root = document.querySelector('.root');
-const app = <App />;
-ReactDom.render(app, root);
